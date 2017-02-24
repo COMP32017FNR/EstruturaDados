c191c9dc2b1e357aedc3606a7949603ddb68c01c
LeonardoEvangelista1998 <leonardodaraujo@hotmail.com>
2017-02-24 11:51:16 -0300

    none

--- PSO/src/PSOProcess.java
+++ PSO/src/PSOProcess.java
@@ -1,22 +1,9 @@
 
-
 public class PSOProcess {
-	public static double loc[]=new double[100];
+	public static double loc[] = new double[100];
 
 	public static void starProcess(int S, int dimensão) {
-	
-		Particle particle = new Particle();
-		for (int i = 0; i < S; i++) {
-			boolean n = Main.random.nextBoolean();
-			for (int j = 0; j < dimensão; j++) {
-				if(n==true){
-					
-				}else{
-					
-				}
-				}
-			}
 
-		}
 	}
 
+}
